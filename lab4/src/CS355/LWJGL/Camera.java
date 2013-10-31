f0964a894aabc8d5062ab38de0aea4fd751440a5
Michael Eagar <meagar@proofpoint.com>
2013-10-30 21:48:56 -0600

    Final commit for lab 4.

---
+++ lab4/src/CS355/LWJGL/Camera.java
@@ -0,0 +1,124 @@
+package CS355.LWJGL;
+
+public class Camera
+{
+	// Variables
+	private Point3D location;
+	private float yaw, pitch, roll;
+
+	// Constructor(s)
+	public Camera()
+	{
+		this.location = new Point3D(0.0d, 0.0d, 0.0d);
+		this.yaw = 0.0f;
+		this.pitch = 0.0f;
+		this.roll = 0.0f;
+	}
+	public Camera(Point3D location)
+	{
+		this.location = location;
+		this.yaw = 0.0f;
+		this.pitch = 0.0f;
+		this.roll = 0.0f;
+	}
+
+	// Methods
+	public Point3D getLocation()
+	{
+		return this.location;
+	}
+
+	public void setLocation(Point3D location)
+	{
+		this.location = location;
+	}
+
+	public float getYaw()
+	{
+		return this.yaw;
+	}
+
+	public void yaw(float yaw)
+	{
+		this.yaw += yaw;
+	}
+
+	public void setYaw(float yaw)
+	{
+		this.yaw = yaw;
+	}
+
+	public float getPitch()
+	{
+		return this.pitch;
+	}
+
+	public void pitch(float pitch)
+	{
+		this.pitch += pitch;
+	}
+
+	public void setPitch(float pitch)
+	{
+		this.pitch = pitch;
+	}
+
+	public float getRoll()
+	{
+		return this.roll;
+	}
+
+	public void roll(float roll)
+	{
+		this.roll += roll;
+	}
+
+	public void setRoll(float roll)
+	{
+		this.roll = roll;
+	}
+
+	// moves the camera up or down ignoring current rotation
+	public void changeAltitude(float distance)
+	{
+		this.location.y += distance;
+	}
+
+	// moves the camera forward relative to its current rotation (yaw)
+	public void moveForward(float distance)
+	{
+		this.location.x -= distance * (float) Math.sin(Math.toRadians(yaw));
+		this.location.z += distance * (float) Math.cos(Math.toRadians(yaw));
+	}
+
+	// moves the camera backward relative to its current rotation (yaw)
+	public void moveBackward(float distance)
+	{
+		this.location.x += distance * (float) Math.sin(Math.toRadians(yaw));
+		this.location.z -= distance * (float) Math.cos(Math.toRadians(yaw));
+	}
+
+	// strafe the camera left relative to its current rotation (yaw)
+	public void strafeLeft(float distance)
+	{
+		this.location.x -= distance * (float) Math.sin(Math.toRadians(yaw - 90));
+		this.location.z += distance * (float) Math.cos(Math.toRadians(yaw - 90));
+	}
+
+	// strafe the camera right relative to its current rotation (yaw)
+	public void strafeRight(float distance)
+	{
+		this.location.x -= distance * (float) Math.sin(Math.toRadians(yaw + 90));
+		this.location.z += distance * (float) Math.cos(Math.toRadians(yaw + 90));
+	}
+	
+	// combined left/right strafe method. strafe's left/right relative to camera's current rotation (yaw)
+	public void strafe(float distance)
+	{
+		int negate = distance >= 0 ? -1 : 1;
+		float offset = 90 * negate;
+
+		this.location.x += negate * distance * (float) Math.sin(Math.toRadians(yaw + offset));
+		this.location.z += -negate * distance * (float) Math.cos(Math.toRadians(yaw + offset));
+	}
+}
--- lab4/src/CS355/LWJGL/StudentLWJGLController.java
+++ lab4/src/CS355/LWJGL/StudentLWJGLController.java
@@ -8,7 +8,11 @@ package CS355.LWJGL;
 //Therefore, if a command appears in this list, you probably need it.
 //If it doesn't appear in this list, you probably don't.
 //Of course, your mileage may vary. Don't feel restricted by this list of imports.
+import java.util.Iterator;
+
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.test.applet.OpenGL;
+
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_LINES;
 import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
@@ -33,10 +37,19 @@ import static org.lwjgl.util.glu.GLU.gluPerspective;
  */
 public class StudentLWJGLController implements CS355LWJGLController 
 {
+	// Variables
+	private Camera camera = new Camera(new Point3D(0f, -5f, -25f));
+	private final float zNearPlane = 0.001f;
+	private final float zFarPlane = 1000.0f;
+	private final float aspectRatio = (float) LWJGLSandbox.DISPLAY_WIDTH / (float) LWJGLSandbox.DISPLAY_HEIGHT;
+	private final float fieldOfView = 45.0f;
+	private final float orthoNeg = -10.0f;
+	private final float orthoPos = 10.0f;
+	private boolean perspective = true;
 
 	// This is a model of a house.
 	// It has a single method that returns an iterator full of Line3Ds.
-	// A "Line3D" is a wrapper class around two Point2Ds.
+	// A "Line3D" is a wrapper class around two Point3Ds.
 	// It should all be fairly intuitive if you look at those classes.
 	// If not, I apologize.
 	private final WireFrame model = new HouseModel();
@@ -46,7 +59,18 @@ public class StudentLWJGLController implements CS355LWJGLController
 	@Override
 	public void resizeGL()
 	{
-
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		if (this.perspective)
+		{
+			gluPerspective(this.fieldOfView, this.aspectRatio, this.zNearPlane, this.zFarPlane);
+		}
+		else
+		{
+			glOrtho(this.orthoNeg, this.orthoPos, this.orthoNeg, this.orthoPos, this.zNearPlane, this.zFarPlane);
+		}
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
 	}
 
 	@Override
@@ -62,9 +86,59 @@ public class StudentLWJGLController implements CS355LWJGLController
 	@Override
 	public void updateKeyboard()
 	{
-		if (Keyboard.isKeyDown(Keyboard.KEY_W))
+		if (Keyboard.isKeyDown(Keyboard.KEY_W) || Keyboard.isKeyDown(Keyboard.KEY_UP) || Keyboard.isKeyDown(Keyboard.KEY_NUMPAD8))
 		{
-			System.out.println("You are pressing W!");
+			this.camera.moveForward(1.0f);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_A) || Keyboard.isKeyDown(Keyboard.KEY_LEFT) || Keyboard.isKeyDown(Keyboard.KEY_NUMPAD4))
+		{
+			this.camera.strafe(1.0f);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_S) || Keyboard.isKeyDown(Keyboard.KEY_DOWN) || Keyboard.isKeyDown(Keyboard.KEY_NUMPAD2))
+		{
+			this.camera.moveBackward(1.0f);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_D) || Keyboard.isKeyDown(Keyboard.KEY_RIGHT) || Keyboard.isKeyDown(Keyboard.KEY_NUMPAD6))
+		{
+			this.camera.strafe(-1.0f);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_Q) || Keyboard.isKeyDown(Keyboard.KEY_NUMPAD7))
+		{
+			this.camera.yaw(-1.0f);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_E) || Keyboard.isKeyDown(Keyboard.KEY_NUMPAD9))
+		{
+			this.camera.yaw(1.0f);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_R) || Keyboard.isKeyDown(Keyboard.KEY_SUBTRACT))
+		{
+			this.camera.changeAltitude(-1.0f);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_F) || Keyboard.isKeyDown(Keyboard.KEY_ADD))
+		{
+			this.camera.changeAltitude(1.0f);
+		}
+		// if (Keyboard.isKeyDown(Keyboard.KEY_Z) || Keyboard.isKeyDown(Keyboard.KEY_NUMPAD1))
+		// {
+		// this.camera.roll(1.0f);
+		// }
+		// if (Keyboard.isKeyDown(Keyboard.KEY_C) || Keyboard.isKeyDown(Keyboard.KEY_NUMPAD3))
+		// {
+		// this.camera.roll(-1.0f);
+		// }
+		if (Keyboard.isKeyDown(Keyboard.KEY_H) || Keyboard.isKeyDown(Keyboard.KEY_NUMPAD5))
+		{
+			this.camera = new Camera(new Point3D(0f, -5f, -25f));
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_O) || Keyboard.isKeyDown(Keyboard.KEY_DIVIDE))
+		{
+			this.perspective = false;
+			this.resizeGL();
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_P) || Keyboard.isKeyDown(Keyboard.KEY_MULTIPLY))
+		{
+			this.perspective = true;
+			this.resizeGL();
 		}
 	}
 
@@ -75,7 +149,42 @@ public class StudentLWJGLController implements CS355LWJGLController
 		// This clears the screen.
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Do your drawing here.
+		// load identity matrix
+		glLoadIdentity();
+
+		// move view to camera
+		this.moveToCamera();
+
+		// Get ready to draw lines
+		glBegin(GL_LINES);
+
+		// Set color
+		glColor3f(0.2f, 1.0f, 0f);
+
+		// Get lines from model
+		Iterator<Line3D> modelIter = this.model.getLines();
+		while (modelIter.hasNext())
+		{
+			// Draw all the lines
+			Line3D line = modelIter.next();
+			glVertex3d(line.start.x, line.start.y, line.start.z);
+			glVertex3d(line.end.x, line.end.y, line.end.z);
+		}
+
+		// End drawing lines
+		glEnd();
+	}
+
+	public void moveToCamera()
+	{
+		Point3D camLoc = this.camera.getLocation();
+		float yaw = this.camera.getYaw();
+		// float roll = this.camera.getRoll();
+
+		glRotatef(yaw, 0.0f, 1.0f, 0.0f);
+		// glRotatef(roll, 0.0f, 0.0f, 1.0f);
+
+		glTranslatef((float) camLoc.x, (float) camLoc.y, (float) camLoc.z);
 	}
 
 }
